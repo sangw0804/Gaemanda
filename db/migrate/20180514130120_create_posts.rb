cd9615c438888a99a8fb423e9d54e625448308b7
SangWoo <sangw0804@likelion.org>
2018-05-14 22:02:21 +0900

    start

---
+++ db/migrate/20180514130120_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.integer :match_id
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
