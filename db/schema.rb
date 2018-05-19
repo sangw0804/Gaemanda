384f0ed3d20dd390a3e2be79c96b9bcf86086f26
“Hyejoojjang” <hyejoojjang@likelion.org>
2018-05-19 23:03:26 +0900

    root page section_1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180515092646) do
+ActiveRecord::Schema.define(version: 20180518141520) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "liker"
